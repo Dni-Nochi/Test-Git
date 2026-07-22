92a35efb3834e7d12af73976188f04fcef3a5772
Adilov Timur <adilovtimur14@gmail.com>
2026-07-22 14:35:29 +0500

    test

--- gitLearn.js
+++ gitLearn.js
@@ -1,3 +1,20 @@
-console.log('Git test');
-console.log('Git test 2');
-console.log('Git test 3');
+class Solution {
+  hasDuplicate(nums) {
+    const seen = new Set();
+
+    for (const num of nums) {
+      if (seen.has(num)) {
+        return true;
+      }
+      console.log(num, nums);
+      seen.add(num);
+    }
+
+    return false;
+  }
+}
+
+const solution = new Solution();
+
+console.log(solution.hasDuplicate([1, 2, 3, 3]));
+console.log(solution.hasDuplicate([1, 2, 3, 4]));
