aa7bc507a87c7a4df21ddaaddf937bbbddcb60f4
Adilov Timur <adilovtimur14@gmail.com>
2026-07-27 12:12:12 +0500

    feat: решал задачи

--- gitLearn.js
+++ gitLearn.js
@@ -1,5 +1,13 @@
-console.log('Сейчас соединим две ветки в одну ');
-console.log('test');
-console.log('asdasdasd');
-console.log('Посмотрим что будет если будут конфликты');
-console.log('gg');
+const userCache = new Map();
+
+function getUserProfile(userName) {
+  if (userCache.has(userName)) {
+    console.log(`Берем из кеша: ${userCache.get(userCache)}`);
+  } else {
+    console.log(`Загружаем с сервера: ${userName}`);
+    userCache.set(userName, { data: 'какие-то данные' });
+    return userCache.get(userName).data;
+  }
+}
+
+console.log(getUserProfile('alex99'));
