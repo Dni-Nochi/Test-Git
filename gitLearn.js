9f68cc529832b7642d312d13bef601400f1b8d30
Adilov Timur <adilovtimur14@gmail.com>
2026-07-30 14:42:24 +0500

    feat

--- gitLearn.js
+++ gitLearn.js
@@ -1,13 +1,2 @@
-const userCache = new Map();
-
-function getUserProfile(userName) {
-  if (userCache.has(userName)) {
-    console.log(`Берем из кеша: ${userCache.get(userCache)}`);
-  } else {
-    console.log(`Загружаем с сервера: ${userName}`);
-    userCache.set(userName, { data: 'какие-то данные' });
-    return userCache.get(userName).data;
-  }
-}
-
-console.log(getUserProfile('alex99'));
+const check = 'text';
+const gg = 'React Routing';
