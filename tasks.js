276fbafe371bb3b522bb92d053cb32cc72a42c59
Adilov Timur <adilovtimur14@gmail.com>
2026-07-30 23:25:43 +0500

    fead

--- tasks.js
+++ tasks.js
@@ -1,59 +1,22 @@
-const nums = [5, 10, 15, 20];
+'use strict';
 
-const sum = nums.reduce((acc, curr) => {
-  return acc + curr;
-}, 0);
-console.log(sum);
+function show() {
+  console.log(this);
+}
 
-const numsTwo = [1, 2, 3, 4];
+show();
 
-const multip = numsTwo.reduce((acc, curr) => {
-  return acc * curr;
-}, 1);
+const user = {
+  name: 'Тима',
+  show: function () {
+    console.log(this.name);
+  },
+};
 
-console.log(multip);
+user.show();
 
-const nums3 = [3, 8, 1, 6];
-
-const needNum = nums3.some((num) => {
-  return num > 5;
-});
-
-console.log(needNum);
-
-const allPlus = nums3.every((num) => {
-  return num > 0;
-});
-
-console.log(allPlus);
-
-const nums4 = [40, 1, 5, 200, 30];
-nums4.sort((a, b) => {
-  return a - b;
-});
-
-console.log(nums4);
-
-nums4.sort((a, b) => {
-  return b - a;
-});
-
-console.log(nums4);
-
-nums4.sort((a, b) => {
-  return a - b;
-});
-
-console.log(nums4);
-
-const cart = [
-  { name: 'Хлеб', price: 200 },
-  { name: 'Молоко', price: 450 },
-  { name: 'Сыр', price: 1200 },
-];
-
-const allPrice = cart.reduce((acc, cartItems) => {
-  return acc + cartItems.price;
-}, 0);
-
-console.log(allPrice);
+const admin = {
+  name: 'Admin',
+  show: user.show,
+};
+admin.show();
