0543f8c996912ce9a50b620b9161bb55c3a3f374
Adilov Timur <adilovtimur14@gmail.com>
2026-08-06 10:09:09 +0500

    feat: added need logic

--- tasks.js
+++ tasks.js
@@ -38,7 +38,5 @@ obj2.showName();
 
 console.log('Пишу из ветки develop');
 console.log('Пишу из ветки develop 2');
-console.log('develop');
 console.log('develop3');
 console.log('develop');
-console.log('test');
