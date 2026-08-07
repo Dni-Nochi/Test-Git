4b1689ce50956571da9a51fefa73c79c7c90fd2d
Adilov Timur <adilovtimur14@gmail.com>
2026-08-07 11:41:34 +0500

    feax: feax error function

--- tasks2.js
+++ tasks2.js
@@ -6,6 +6,6 @@ function sayHello2() {
   console.log('Hi my name is ...chicke chike SLim');
 }
 
-function fatalError() {
-  console.log('ERRROR');
+function noErrorFunction() {
+  console.log('realy no error');
 }
