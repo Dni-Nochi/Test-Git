5c7919c6297b2ba6f12d3b8302471f626cdcdee8
Adilov Timur <adilovtimur14@gmail.com>
2026-08-07 11:03:28 +0500

    feat: i test git command, git rm --cached <file name>

--- tasks.js
+++ tasks.js
@@ -40,3 +40,5 @@ console.log('Пишу из ветки develop');
 console.log('Пишу из ветки develop 2');
 console.log('develop3');
 console.log('develop');
+
+console.log('test, chekc how work git rm --cached <file name>');
