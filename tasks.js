5d395daa3e39668414031e03105e41c294e64138
Adilov Timur <adilovtimur14@gmail.com>
2026-08-11 12:03:20 +0500

    feat: Yesterday i wrote three tasks, today i wrote two, and solve two more

--- tasks.js
+++ tasks.js
@@ -1,17 +1,88 @@
-// Задача с NeetCode Contains Duplicate
-class Solution {
-  hasDuplicate(arr) {
-    const mySet = new Set();
-    for (let i = 0; i < arr.length; i++) {
-      if (mySet.has(arr[i])) {
-        return true;
-      } else {
-        mySet.add(arr[i]);
-      }
-    }
-    return false;
-  }
-}
+// Задача 1: Find Max
+// class FindMax {
+//   constructor(arr) {
+//     this.arr = arr;
+//   }
 
-const giveArr = new Solution();
-console.log(giveArr.hasDuplicate([1, 2, 3]));
+//   findMaxValue() {
+//     let maxValue = this.arr[0];
+//     for (let i = 0; i < this.arr.length; i++) {
+//       console.log(this.arr[i]);
+//       if (this.arr[i] > maxValue) {
+//         maxValue = this.arr[i];
+//       }
+//     }
+//     console.log(maxValue, 'Max V');
+//     return maxValue;
+//   }
+// }
+
+// const test = new FindMax([-5, -2, -10]);
+// test.findMaxValue();
+
+// Задача 2: Count Vowels
+
+// class Vowels {
+//   constructor(str) {
+//     this.str = str.toLowerCase();
+//   }
+
+//   countVowels() {
+//     const vowelsConst = ['a', 'e', 'i', 'o', 'u'];
+//     let countVowelsSummary = 0;
+//     for (let i = 0; i < this.str.length; i++) {
+//       if (vowelsConst.includes(this.str[i])) {
+//         countVowelsSummary++;
+//         console.log(this.str[i]);
+//       }
+//     }
+//     return countVowelsSummary;
+//   }
+// }
+
+// const hello = new Vowels('hello');
+// console.log(hello.countVowels());
+
+// Задача 3: Reverse a String
+
+// class ReverseString {
+//   constructor(str) {
+//     this.str = str;
+//   }
+
+//   reverseStr() {
+//     let reverse = '';
+//     console.log(this.str);
+//     for (let i = this.str.length - 1; i >= 0; i--) {
+//       reverse = reverse + this.str[i];
+//     }
+//     return reverse;
+//   }
+// }
+
+// Задача 4: Most Frequent Character
+
+// class MostCharacter {
+//   constructor(character) {
+//     this.character = character;
+//   }
+
+//   giveMostFrequentCharacter() {
+//     let objChar = {};
+//     for (let i = 0; i < this.character.length; i++) {
+//       if (objChar[this.character[i]] === undefined) {
+//         objChar[this.character[i]] = 1;
+//       } else {
+//         objChar[this.character[i]] = objChar[this.character[i]] + 1;
+//         console.log(objChar[this.character[i]], this.character[i], objChar);
+//       }
+//     }
+//     Object.keys(objChar).forEach((key) => {
+//       console.log(key, objChar, objChar[key]);
+//     });
+//   }
+// }
+
+// const char = new MostCharacter('abccacac');
+
+// char.giveMostFrequentCharacter();
